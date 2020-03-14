4057cf59fa5d7c576ef959cfe14b73a724dce6eb
jie1414 <32653842+jie1414@users.noreply.github.com>
2020-03-14 11:39:18 -0400

    Update du code avec des sparse

--- Test_Code.m
+++ Test_Code.m
@@ -1,13 +1,13 @@
 clc; clearvars;
 
 % Dimensions d'appartement
-Lx=4; %[m]
+Lx=2.4; %[m]
 Ly=2.4;  %[m]
 
 omega = 2.*pi.*10e3;
 
 d_ar=[];tini_ar=[];tinv_ar=[];mem_ar=[];Tm_ar=[];
-fact=1/2;
+fact=1/4;
 d=0.1*fact; %Pas de discrétisation en [m]
 d_ar=[d_ar d];
 Nx=round(Lx/d)+1;
@@ -20,24 +20,23 @@ rho_e = 2.15e9;
 
 tic
 % Initialisation de la source de chaleur et de la conductivité thermique
-S=zeros(Ny,Nx); B=zeros(Ny,Nx); alpha=zeros(Ny,Nx); rho=zeros(Ny,Nx);
+S=sparse(Ny,Nx); B=sparse(Ny,Nx); alpha=sparse(Ny,Nx); rho=sparse(Ny,Nx);
 for i=1:Ny
     y=(i-1)*d;
     for j=1:Nx
         x=(j-1)*d;
         
-%                 if (x>=1.9)&&(x<=1.9+d)&&(y>=0.9)&&(y<=0.9+d)
-%                     % À l'intérieur de l'élément chauffant
-%                     S(i,j)=exp(-1i.*((x-1.8)+(y-1.2).^2));
-%                 else
-%                     % À l'extérieur de l'élément chauffant
-%                     S(i,j)=0;
-%                 end
+                if (x>=1.4)&&(x<=1.4+d)&&(y>=1)&&(y<=1+d)
+                    % À l'intérieur de l'élément chauffant
+                    S(i,j)=1;
+                else
+                    % À l'extérieur de l'élément chauffant
+                    S(i,j)=0;
+                end
         
-        S(i,j)=exp(-1i*(((x-1.8).^2) + ((y-1.2).^2)));
         
         % Délimitation des espaces
-        if (x>=2)&&(x<=2.5)&&(y>=1)&&(y<=1.5)
+        if (x>=1.5)&&(x<=2)&&(y>=1)&&(y<=1.5)
             % Bois
             B(i,j) = 640.72;
             alpha(i,j) = alpha_b;
@@ -52,11 +51,11 @@ for i=1:Ny
     end
 end
 
-M=zeros(Nx*Ny,Nx*Ny);
-b=zeros(Nx*Ny,1);
+M=sparse(Nx*Ny,Nx*Ny);
+b=sparse(Nx*Ny,1);
 
-ke = (rho_e.*(alpha_e - 1i.*omega));
-kb = (rho_b.*(alpha_b - 1i.*omega));
+ke = (rho_e.*(alpha_e + 1i.*omega));
+kb = (rho_b.*(alpha_b + 1i.*omega));
 
 for i=1:Ny
     y=(i-1)*d;
@@ -66,10 +65,10 @@ for i=1:Ny
         pl=(i-1)*Nx+j;
         
         if ((i>1)&(i<Ny))&&((j>1)&(j<Nx)) ...
-                &~((x==2) & ((y>=1)&(y<=(2)))) ...
-                &~((x==(3)) & ((y>=1)&(y<=(2)))) ...
-                &~((y==1) & ((x>=2)&(x<=(3)))) ...
-                &~((y==(2)) & ((x>=2)&(x<=(3))))
+                &~((x==1.5) & ((y>=1)&(y<=(1.5)))) ...
+                &~((x==(2)) & ((y>=1)&(y<=(1.5)))) ...
+                &~((y==1) & ((x>=2)&(x<=(1.5)))) ...
+                &~((y==(1.5)) & ((x>=2)&(x<=(2))))
             % noeud qui est strictement à l'intérieur de la cellule de simulation
             pc=pl;                      M(pl,pc)=(omega.^2./B(i,j).*d.^2.*rho(i,j) + 1i.*omega.*alpha(i,j).*d.^2.*rho(i,j) - 4); % contribution de noeud (i,j)
             pc=(i-1)*Nx+j-1;            M(pl,pc)=1; % contribution de noeud (i,j-1)
@@ -107,7 +106,7 @@ for i=1:Ny
             b(pl)=0;
             
             % Conditions entre milieu
-        elseif (x==2) & ((y>=1)&(y<=(2)))
+        elseif (x==1.5) & ((y>=1)&(y<=(1.5)))
             %Le long de la surface entre mur et appartement gauche
             pc=(i-1)*Nx+j-2;            M(pl,pc)=kb; % contribution de noeud (i,M-2)
             pc=(i-1)*Nx+j-1;            M(pl,pc)=-4*kb; % contribution de noeud (i,M-1)
@@ -115,7 +114,7 @@ for i=1:Ny
             pc=(i-1)*Nx+j+1;            M(pl,pc)=-4*ke; % contribution de noeud (i,M+1)
             pc=(i-1)*Nx+j+2;            M(pl,pc)=ke; % contribution de noeud (i,M+2)
             b(pl)=0;
-        elseif (x==(3)) & ((y>=1)&(y<=(2)))
+        elseif (x==(2)) & ((y>=1)&(y<=(1.5)))
             %Le long de la surface entre mur et appartement droite
             pc=(i-1)*Nx+j-2;            M(pl,pc)=ke; % contribution de noeud (i,M-2)
             pc=(i-1)*Nx+j-1;            M(pl,pc)=-4*ke; % contribution de noeud (i,M-1)
@@ -123,7 +122,7 @@ for i=1:Ny
             pc=(i-1)*Nx+j+1;            M(pl,pc)=-4*kb; % contribution de noeud (i,M+1)
             pc=(i-1)*Nx+j+2;            M(pl,pc)=kb; % contribution de noeud (i,M+2)
             b(pl)=0;
-        elseif (y==1) & ((x>=2)&(x<=(3)))
+        elseif (y==1) & ((x>=1.5)&(x<=(2)))
             %Le long du plancher entre mur et appartement (sol)
             pc=(i-3)*Nx+j;              M(pl,pc)=kb; % contribution de noeud (M-2,j)
             pc=(i-2)*Nx+j;              M(pl,pc)=-4*kb; % contribution de noeud (M-1,j)
@@ -131,7 +130,7 @@ for i=1:Ny
             pc=(i)*Nx+j;                M(pl,pc)=-4*ke; % contribution de noeud (M+1,j)
             pc=(i+1)*Nx+j;              M(pl,pc)=ke; % contribution de noeud (M+2,j)
             b(pl)=0;
-        elseif (y==(2)) & ((x>=2)&(x<=(3)))
+        elseif (y==(1.5)) & ((x>=1.5)&(x<=(2)))
             %Le long du plafond entre mur et appartement (haut)
             pc=(i-3)*Nx+j;              M(pl,pc)=ke; % contribution de noeud (M-2,j)
             pc=(i-2)*Nx+j;              M(pl,pc)=-4*ke; % contribution de noeud (M-1,j)
@@ -177,7 +176,7 @@ axis tight
 
 figure(3)
 h=pcolor((0:d:Lx),(0:d:Ly),log(abs(Tr)));set(h,'LineStyle','none')
-colormap(jet);
+colormap(hot);
 colorbar;
 xlabel('x [m]'); ylabel('y [m]'); title('T(x,y) [^oC]')
 axis equal
