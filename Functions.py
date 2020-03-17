9f98939eaa199da490968b403469fe292a0d0588
Pierre-Luc <pierreluc.theriault@gmail.com>
2020-03-17 14:38:46 -0400

    Création de la version code

---
+++ Functions.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Importer ce qui est nécessaire pour les fonctions
+
+def Fonction_De_Base(JM):
+    return JM+1
--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 
 
 import numpy as np
+from Functions import Fonction_De_Base
+
+
+# Paramètres de la simulations
+
+
+
+
+
+Afficher_graphique=
+# Main:
+
+
+if __name__ == "__main__":
+
+    Fonction_De_Base(1)
+
+    if Afficher_graphique == True:
+        Fonction_Graphique():
